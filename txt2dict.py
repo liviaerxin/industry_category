7162d4cdf133c80a989f639225249fe7de4e1603
unknown <1yue8haogaoqi@gmail.com>
2015-03-03 17:55:05 +0800

    all

--- txt2dict.py
+++ txt2dict.py
@@ -1,10 +1,16 @@
-import os
+'''
+usage: load txt file to the mongodb database as such format we can see in file './data_sample.json'  
+'''
+
+
+import os,sys
+print "sys code is:", sys.getdefaultencoding() 
 import json
 c_path=os.path.abspath('.')
 HY_list = [] #global list to store kinds of hangye
 
 txtfiles = [x for x in os.listdir(c_path) if os.path.isfile(x) and os.path.splitext(x)[1] == '.txt']
-print txtfiles[1].decode('utf-8')
+print txtfiles[1].decode('gb2312')   #windows gb2312, osx utf-8
 print '-------------------'
 for x in txtfiles:
     global HY_list
@@ -21,16 +27,17 @@ for x in txtfiles:
     HY_list.append(temp) #add the dict record
 f.close()
 
-print HY_list
+#print HY_list   utf8
 
 f = open('HY_list.json', 'w')
 
-str_json = json.dumps(HY_list, indent = 4)
+str_json = json.dumps(HY_list, indent = 4, encoding = "gb2312")  #windows gb2312, osx utf-8
 str_json = str_json.decode('raw_unicode_escape')
-str_json = str_json.encode('utf-8')
-print str_json.__class__
+str_json = str_json.encode('gb2312') #windows gb2312, osx utf-8
+print str_json
 f.write(str_json)
 f.close()
+
 
 from pymongo import MongoClient
 mClient=MongoClient('54.169.18.100', 27017)
@@ -38,12 +45,18 @@ db=mClient.test  #test database
 hy=db.hy   #hy collection
 
 f = open('HY_list.json', 'r')
-unistring = f.read()
-l = json.loads(unistring)
-print l
-hy.insert(l)
+string = f.read()   #string encoded as gb2312 in windows, utf-8 in osx 
+print string
+l = json.loads(string, encoding = "gb2312")
+
+print l[1]
+hy.insert(l)  #l contains all hy dict l=[hy1,hy2,...] hy1={'HY':..,'sz0001':{'name':.,..},..}
 
 f.close()
+
+
+
+
 '''
 f = open('HY_list.json', 'r')
 
